00bacbb17c80665bed972d93560f6ad836651cfb
Chin-I Feng <c.feng@outlook.de>
2025-08-21 01:34:32 +0200

    chore: make build-deploy-info.sh executable

--- build-deploy-info.sh
+++ build-deploy-info.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-echo "window.lastDeploy = '$(date "+%Y-%m-%d %H:%M:%S")';" > deploy-info.js
+#!/usr/bin/env bash
+set -e
+
+# Build timestamp in UTC (ISO-like)
+BUILD_TIME="$(date -u +"%Y-%m-%d %H:%M:%S UTC")"
+
+# Netlify provides the commit hash as $COMMIT_REF
+COMMIT="${COMMIT_REF:-unknown}"
+
+# Write file available in browser
+cat > deploy-info.js <<EOF
+window.lastDeploy = "${BUILD_TIME}";
+window.lastCommit = "${COMMIT}";
+EOF
+
+echo "Wrote deploy-info.js with time=${BUILD_TIME}, commit=${COMMIT}"
